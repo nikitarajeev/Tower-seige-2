fdc569fe1fef61f9781c8028e47b766268828999
nikitarajeev <69901496+nikitarajeev@users.noreply.github.com>
2020-10-09 20:26:44 +0530

    Add files via upload

---
+++ CyanBlock.js
@@ -0,0 +1,30 @@
+class Box2 {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: false
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      this.Visiblity = 255;
+      World.add(world, this.body);
+    }
+    display(){
+      if(this.body.speed < 3){
+        var pos =this.body.position;
+        push();
+        rectMode(CENTER);
+        fill("cyan");
+        rect(pos.x, pos.y, this.width, this.height);
+        pop();
+         }
+         else{
+           World.remove(world, this.body);
+           push();
+           this.Visiblity = this.Visiblity - 5;
+          // tint(255,this.Visiblity);
+           //rect(this.body.position.x, this.body.position.y, 50, 50);
+           pop();
+         }
+        }
+  };
